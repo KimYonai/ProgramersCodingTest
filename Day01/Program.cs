3728b27049a97eaf0b182cb6eb437fe69bd844c6
KimYonai <msb0714@gmail.com>
2024-08-05 15:54:29 +0900

    Initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Day01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //문제1: 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요.
+            //public int solution(int num1, int num2)                   //1. solution 함수에서 정수 num1과 num2 선언
+            //{
+            //    int answer = num1 + num2;                             //2. num1과 num2를 더하기 연산 실행
+            //    return answer;                                        //3. num1과 num2의 더하기 연산값 도출
+            //
+            //    Console.WriteLine($"Answer: {num1 + num2}");          //4. num1과 num2의 더하기 연산값을 출력
+            //}
+
+            //문제2: 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution 함수를 완성해주세요.
+            //public int solution(int num1, int num2)                 //1. solution 함수에서 정수 num1과 num2 선언
+            //{
+            //    int answer = num1 - num2;                           //2. num1과 num2를 마이너스 연산 실행
+            //    return answer;                                      //3. num1과 num2의 마이너스 연산값 도출
+            //
+            //    Console.WriteLine($"Answer: {num1 - num2}");        //4. num1과 num2의 마이너스 연산값을 출력
+            //}
+
+            //문제3: 정수 num1, num2가 매개변수 주어집니다.num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+            //public int solution(int num1, int num2)                //1. solution 함수에서 정수 num1과 num2 선언    
+            //{
+            //    int answer = num1 * num2;                          //2. num1과 num2를 곱연산 실행
+            //    return answer;                                     //3. num1과 num2의 곱 연산값 도출
+            //
+            //    Console.WriteLine($"Answer: {num1 * num2}");       //4. num1과 num2의 곱 연산값을 출력
+            //}
+
+            //문제4: 정수 num1, num2가 매개변수 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+            //public int solution(int num1, int num2)                  //1. solution 함수에서 정수 num1과 num2 선언
+            //{
+            //    int answer = num1 / num2;                            //2. num1과 num2를 나누기 연산 실행
+            //    return answer;                                       //3. num1과 num2의 나누기 연산으로 인한 몫만 연산값으로 출력
+            //}
+
+            //문제5: 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1, 다르면 -1을 return하도록 solution 함수를 완성해주세요.
+            //public int solution(int num1, int num2)       //1. solution 함수에서 정수 num1과 num2 선언
+            //{
+            //    int answer;                               //2. 결과값을 받을 answer을 int로 변수 선언
+            //    if (num1 == num2)                         
+            //    {
+            //        answer = 1;                           //3. num1과 num2가 같을 때 answer(결과값)을 1로 설정
+            //    }
+            //    else
+            //    {
+            //        answer = -1;                          //4. 나머지 경우(num1과 num2가 같지 않을 때)에는 answer(결과값)을 -1로 설정
+            //    }
+            //    return answer;                            //5. 조건에 따른 결과값 도출
+            //}
+
+
+        }
+    }
+}
